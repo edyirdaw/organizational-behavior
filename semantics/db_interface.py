d9b09f522111b8cef0c712e2e64d923a36328b41
Eyob Yirdaw <edyirdaw@gmail.com>
2018-10-08 11:40:32 +0300

    init commit

---
+++ semantics/db_interface.py
@@ -0,0 +1,84 @@
+# Tested on python2.7
+
+import psycopg2
+import psycopg2.extras
+import logging
+import os
+
+
+class DbInterface:
+
+    db_dict = {'local':['OB_LOCAL_HOSTNAME','OB_LOCAL_USERNAME','OB_LOCAL_PASSWORD'],'remote_1':['OB_REMOTE_1_HOSTNAME','OB_REMOTE_1_USERNAME','OB_REMOTE_1_PASSWORD']}
+
+    def __init__(self,db):
+
+
+        self.db = 0
+        self.connect(db)
+
+
+    def __del__(self):
+
+        # Close sql connection
+
+        try:
+            self.conn.close()
+            print('Connection to postgresql closed.')
+        except Exception as e:
+            logging.exception("message")
+            print('Error closing connection to postgresql.')
+
+    def connect(self,db):
+
+        # Create a connection object
+
+        hostname = os.environ[self.db_dict[db][0]]
+        username = os.environ[self.db_dict[db][1]]
+        password = os.environ[self.db_dict[db][2]]
+
+        database = os.environ['OB_ALL_DATABASE']
+        port = os.environ['OB_ALL_PORT']
+
+        try:
+            self.conn = psycopg2.connect(host=hostname, port=port, user=username, password=password, dbname=database)
+            print('Connection to postgresql opened.')
+
+        except Exception as e:
+            logging.exception("message")
+
+        self.conn.autocommit = True
+        self.cursor = self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+
+    def test(self):
+
+        try:
+            self.cursor.execute("""SELECT * from channel""")
+            rows = self.cursor.fetchall()
+            for row in rows:
+                print(row)
+            print(type(rows))
+            print(type(rows[0]))
+            print(rows[0]['id'])
+        except Exception as e:
+            logging.exception("message")
+
+
+
+
+def run_test():
+
+    d = DbInterface('remote_1')
+    # d = DbInterface('local')
+    d.test()
+
+
+
+__end__ = '__end__'
+
+
+if __name__ == '__main__':
+
+    run_test()
+
+    pass
---
+++ semantics/sentiment_analysis.py
@@ -0,0 +1,31 @@
+# Tested on python2.7
+
+
+class SentimentAnalysis:
+
+
+
+    def __init__(self):
+
+
+        pass
+
+
+    def create_sentiment(self):
+
+        pass
+
+
+
+
+
+
+
+
+__end__ = '__end__'
+
+
+if __name__ == '__main__':
+
+
+    pass

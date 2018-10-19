5572e769ea362c95acbbc9a3066f30a53919b777
Eyob Yirdaw <edyirdaw@gmail.com>
2018-10-19 18:02:51 +0300

    exp on preprocessing step

--- semantics/topic_analysis.py
+++ semantics/topic_analysis.py
@@ -43,6 +43,25 @@ def run_1():
     pass
 
 
+def test_preprocessing():
+
+    root_folder = str(pathlib.Path(os.path.abspath('')).parents[1])+'/appData/'
+
+    pclean.file_parts_number=9
+    pclean.file_dict = root_folder + 'plsa/dict/test_dict'
+    pclean.source_texts = root_folder + 'plsa/extracted/*.txt'
+    pclean.output_dir = root_folder + 'plsa/cleaned/'
+
+
+    # Do cleansing on the data and turing it to bad-of-words model
+    pclean.pre_pro()
+
+    # Train using PLSA
+    pplsa.folder = pclean.output_dir[:-1]
+    pplsa.dict_path = pclean.file_dict
+    pplsa.folder = root_folder + 'plsa/cleaned'
+    pplsa.main()
+
 
 
 
@@ -56,6 +75,7 @@ __end__ = '__end__'
 
 if __name__ == '__main__':
 
-    run_1()
+    # run_1()
+    test_preprocessing()
 
     pass
